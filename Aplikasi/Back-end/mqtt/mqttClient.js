6863ba81d30ad4261aef047215f23713e29decaa
Naufal <naufal.zydan05@gmail.com>
2026-04-06 23:17:55 +0700

    update backend2

--- Aplikasi/Back-end/mqtt/mqttClient.js
+++ Aplikasi/Back-end/mqtt/mqttClient.js
@@ -2,6 +2,7 @@ const mqtt = require("mqtt");
 
 const broker = "mqtt://broker.hivemq.com";
 const topic = "sterilisasi/data";
+const Data = require("../models/data");
 
 const client = mqtt.connect(broker);
 
@@ -18,19 +19,37 @@ client.on("connect", () => {
   });
 });
 
-// menerima pesan
-client.on("message", (topic, message) => {
-  const data = message.toString();
-  console.log("Data masuk:", data);
+// menerima pesan (SATU SAJA)
+client.on("message", async (topic, message) => {
+  const raw = message.toString();
+  console.log("Data masuk:", raw);
 
+  // simpan raw data
   lastData = {
     topic: topic,
-    value: data,
+    value: raw,
     time: new Date(),
   };
+
+  try {
+    const data = JSON.parse(raw);
+
+    console.log("Data valid:", data);
+
+    await new Data({
+      suhu: data.suhu,
+      tekanan: data.tekanan,
+      action: data.action,
+      waktu: data.waktu,
+      device: data.Device,
+    }).save();
+
+  } catch (error) {
+    console.error("JSON tidak valid ❌:", raw);
+  }
 });
 
-// export agar bisa dipakai di server.js
+// export
 module.exports = {
   client,
   getLastData: () => lastData,
